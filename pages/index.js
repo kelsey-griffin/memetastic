615b8309f1e72d97fe5690163c55e43f4187a93d
bmark88 <bradley.mark@hotmail.com>
2020-07-11 12:17:39 -0400

    Add Head component with app script

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
-import Link from 'next/link'
-import { Start, End } from '../components/start'
+import Head from 'next/head';
+import Link from 'next/link';
+import { Start, End } from '../components/start';
 
 export default function Home() {
   return (
-    <div>
-      <div>Meme Myself & I</div>
-      <Start>START</Start>
-      <End>END</End>
-      <Link href="/cards"><a>Link to Meme Cards</a></Link>
-    </div>
+      <div>
+        <Head>
+          <title>Meme Myself & I</title>
+          <script type="text/javascript" src="static/app.js"/>
+          <meta 
+            name="viewport" 
+            content="initial-scale=1.0, width=device-width" 
+          />
+        </Head>
+        <Start>START</Start>
+        <End>END</End>
+        <Link href="/cards"><a>Link to Meme Cards</a></Link>
+      </div>
   )
 }
