a18412b919c337121c2039dcb071af082ea6fcb4
Kelsey Griffin <kjulegriffin@gmail.com>
2020-07-11 18:21:47 -0400

    update state with random cards on start

--- components/Game.jsx
+++ components/Game.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Board from "./Board";
 import Card from "./Card";
 import Hand from "./Hand";
@@ -6,24 +6,33 @@ import MemeLord from "../components/MemeLord";
 import { Start, End } from "../components/start";
 import { deck, FullDeck } from "../pages/cards";
 
+const drawCards = num => {
+  const newHand = [];
+
+  // newHand gets 3 random cards
+  for (let i = 0; i < num; i++) {
+    const randomNumber = Math.floor(Math.random() * deck.length);
+    newHand.push(deck[randomNumber]);
+  }
+
+  return newHand;
+};
+
 const Game = props => {
-  const [game, setGame] = useState({
-    p1: { points: 0, hand: [] },
-    p2: { points: 0, hand: [] },
-  });
+  const [p1State, setP1State] = useState({ points: 0, hand: [] });
+  const [p2State, setP2State] = useState({ points: 0, hand: [] });
 
   const initialDraw = () => {
-    console.log("fullDeck ==>", fullDeck);
-    //get 3 random cards
-    // setGame(prev => ({
-    //   ...prev.p1,
-    //   hand: [],
-    // }));
+    const p1Hand = drawCards(3);
+    const p2Hand = drawCards(3);
+
+    setP1State({ ...p1State, hand: p1Hand });
+    setP2State({ ...p2State, hand: p2Hand });
   };
+
   return (
     <>
-      {/* <Start id="start-game" disabled={false} onClick={initialDraw}> */}
-      <Start id="start-game" disabled={false} onClick={initialDraw}>
+      <Start id="start-game" disabled={false} initialDraw={initialDraw}>
         START
       </Start>
       <End id="end-game" disabled={true}>
@@ -38,7 +47,7 @@ const Game = props => {
       </Board>
       <Board id="hand-1" className="board">
         Player 1's Hand
-        <Hand cards={game.p1.hand} />
+        <Hand cards={p1State.hand} />
         <Card draggable="true" id="fdsa" className="card" holder="1">
           Card 2
         </Card>
@@ -52,7 +61,7 @@ const Game = props => {
       </Board>
       <Board id="hand-2" className="board">
         Player 2's Hand
-        <Hand cards={game.p2.hand} />
+        <Hand cards={p2State.hand} />
         {/* <Card draggable="true" id="fdsa" className="card">
           Card 2
         </Card> */}
