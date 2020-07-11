a6890be8225abb3983ab77fa902331c115ede33f
Kelsey Griffin <kjulegriffin@gmail.com>
2020-07-11 19:35:05 -0400

    remove console.log

--- components/hand.jsx
+++ components/hand.jsx
@@ -7,7 +7,7 @@ import _uniqueId from "lodash/uniqueId";
 const Hand = ({ cards }) => {
   const cardList = cards.map((card, index) => {
     const [id] = useState(_uniqueId("card-"));
-    console.log("in hand==> ", card);
+    // console.log("in hand==> ", card);
     return (
       <Card key={index} id={id} className="card" draggable="true" holder="1">
         {card.memeName}
