7d9a887e2befe3a51a3a564bcf5c6284d4fcf319
Kelsey Griffin <kjulegriffin@gmail.com>
2020-07-11 19:12:19 -0400

    refactor initialdraw for reusability

--- components/Game.jsx
+++ components/Game.jsx
@@ -2,25 +2,34 @@ import React, { useState, useEffect } from "react";
 import Board from "./Board";
 import Card from "./Card";
 import Hand from "./Hand";
+import Draw from "./draw";
 import MemeLord from "../components/MemeLord";
 import { Start, End } from "../components/start";
 import { deck, FullDeck } from "../pages/cards";
 
-const drawCards = num => {
-  const newHand = [];
-
-  // newHand gets 3 random cards
-  for (let i = 0; i < num; i++) {
-    const randomNumber = Math.floor(Math.random() * deck.length);
-    newHand.push(deck[randomNumber]);
-  }
-
-  return newHand;
-};
-
 const Game = props => {
   const [p1State, setP1State] = useState({ points: 0, hand: [] });
   const [p2State, setP2State] = useState({ points: 0, hand: [] });
+
+  const drawCards = num => {
+    const newHand = [];
+
+    // newHand gets 3 random cards
+    for (let i = 0; i < num; i++) {
+      const randomNumber = Math.floor(Math.random() * deck.length);
+      newHand.push(deck[randomNumber]);
+    }
+
+    return newHand;
+  };
+
+  const resetHands = () => {
+    // e.preventDefault();
+    console.log("clicked reset");
+    setP1State({ ...p1State, hand: [] });
+    setP2State({ ...p2State, hand: [] });
+    console.log("resetHands@@@@");
+  };
 
   const initialDraw = () => {
     const p1Hand = drawCards(3);
@@ -28,6 +37,7 @@ const Game = props => {
 
     setP1State({ ...p1State, hand: p1Hand });
     setP2State({ ...p2State, hand: p2Hand });
+    console.log("initial Draw@@@");
   };
 
   return (
@@ -35,36 +45,25 @@ const Game = props => {
       <Start id="start-game" disabled={false} initialDraw={initialDraw}>
         START
       </Start>
-      <End id="end-game" disabled={true}>
+      <End id="end-game" disabled={false} resetHands={resetHands}>
         END
       </End>
+      <Draw drawCards={drawCards} />
       <MemeLord identity="1"></MemeLord>
       <Board id="board-1" className="board">
         Player 1's Board
-        <Card draggable="true" id="asdf" className="card" holder="1">
-          Card 1
-        </Card>
       </Board>
       <Board id="hand-1" className="board">
         Player 1's Hand
         <Hand cards={p1State.hand} />
-        <Card draggable="true" id="fdsa" className="card" holder="1">
-          Card 2
-        </Card>
       </Board>
       <MemeLord identity="2"></MemeLord>
       <Board id="board-2" className="board">
         Player 2's Board
-        {/* <Card draggable="true" id="asdf" className="card">
-          Card 1
-        </Card> */}
       </Board>
       <Board id="hand-2" className="board">
         Player 2's Hand
         <Hand cards={p2State.hand} />
-        {/* <Card draggable="true" id="fdsa" className="card">
-          Card 2
-        </Card> */}
       </Board>
     </>
   );
