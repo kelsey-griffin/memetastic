7149ee0e2e611c5b51dc610355a1e3d9c18814d9
Kelsey Griffin <kjulegriffin@gmail.com>
2020-07-11 16:03:09 -0400

    remove react to Game.jsx

--- static/app.js
+++ static/app.js
@@ -1,29 +1,33 @@
-import react, { useState } from "react";
+document.addEventListener("DOMContentLoaded", function (event) {
+  const start = document.getElementById("start-game");
+  const end = document.getElementById("end-game");
+  let currentMemeLord = "1";
 
-const App = () => {
-  const { hands, SetHands } = useState({ p1: [], p2: [] });
+  document.addEventListener("click", e => {
+    if (e.target.id === "start-game") {
+      console.log("game is starting");
 
-  document.addEventListener("DOMContentLoaded", function (event) {
-    const start = document.getElementById("start-game");
-    const end = document.getElementById("end-game");
-    let currentMemeLord = "1";
+      //disable start button and enable end button
+      e.target.disabled = true;
+      end.disabled = !end.disabled;
 
-    document.addEventListener("click", e => {
-      if (e.target.id === "start-game") {
-        console.log("game is starting");
+      //clear all hands
+      //add cards to players' hands
+    } else if (e.target.id === "end-game") {
+      console.log("game is ending");
 
-        //disable start button and enable end button
-        e.target.disabled = true;
-        end.disabled = !end.disabled;
-
-        //add cards to players' hands
-      } else if (e.target.id === "end-game") {
-        console.log("game is ending");
-
-        //disable end button and enable start button
-        e.target.disabled = true;
-        start.disabled = !start.disabled;
-      }
-    });
+      //disable end button and enable start button
+      e.target.disabled = true;
+      start.disabled = !start.disabled;
+    }
   });
-};
+
+  document.addEventListener("dragend", e => {
+    console.log("e.target", e.target);
+    console.log(e.target.parentElement.id);
+
+    if (e.target.parentElement.id === "board") {
+      e.target.draggable = false;
+    }
+  });
+});
