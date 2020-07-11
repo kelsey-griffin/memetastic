7f39064a32a46235891891575ac30ff971f8d769
Kelsey Griffin <kjulegriffin@gmail.com>
2020-07-11 17:01:24 -0400

    move start/ end buttons into game component

--- components/Game.jsx
+++ components/Game.jsx
@@ -3,15 +3,32 @@ import Board from "./Board";
 import Card from "./Card";
 import Hand from "./Hand";
 import MemeLord from "../components/MemeLord";
+import { Start, End } from "../components/start";
+import { deck, FullDeck } from "../pages/cards";
 
 const Game = props => {
   const [game, setGame] = useState({
     p1: { points: 0, hand: [] },
-    p2: { points: 0, hand: ["abc", "123"] },
+    p2: { points: 0, hand: [] },
   });
 
+  const initialDraw = () => {
+    console.log("fullDeck ==>", fullDeck);
+    //get 3 random cards
+    // setGame(prev => ({
+    //   ...prev.p1,
+    //   hand: [],
+    // }));
+  };
   return (
     <>
+      {/* <Start id="start-game" disabled={false} onClick={initialDraw}> */}
+      <Start id="start-game" disabled={false} onClick={initialDraw}>
+        START
+      </Start>
+      <End id="end-game" disabled={true}>
+        END
+      </End>
       <MemeLord identity="1"></MemeLord>
       <Board id="board-1" className="board">
         Player 1's Board
