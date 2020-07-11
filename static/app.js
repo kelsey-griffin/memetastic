64e91875338adb98aa7b19ef1fdd7f3eaa55afe6
Kelsey Griffin <kjulegriffin@gmail.com>
2020-07-11 14:34:30 -0400

    create overall app component

--- static/app.js
+++ static/app.js
@@ -1,22 +1,29 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  const start = document.getElementById('start-game');
-  const end = document.getElementById('end-game');
-  let currentMemeLord = '1';
+import react, { useState } from "react";
 
-  document.addEventListener('click', (e) => {
-    if (e.target.id === "start-game") {
-      console.log('game is starting');
+const App = () => {
+  const { hands, SetHands } = useState({ p1: [], p2: [] });
 
-      //disable start button and enable end button
-      e.target.disabled = true;
-      end.disabled = !end.disabled;
-    } else if (e.target.id === "end-game") {
-      console.log('game is ending');
+  document.addEventListener("DOMContentLoaded", function (event) {
+    const start = document.getElementById("start-game");
+    const end = document.getElementById("end-game");
+    let currentMemeLord = "1";
 
-      //disable end button and enable start button
-      e.target.disabled = true;
-      start.disabled = !start.disabled;
-    }
+    document.addEventListener("click", e => {
+      if (e.target.id === "start-game") {
+        console.log("game is starting");
+
+        //disable start button and enable end button
+        e.target.disabled = true;
+        end.disabled = !end.disabled;
+
+        //add cards to players' hands
+      } else if (e.target.id === "end-game") {
+        console.log("game is ending");
+
+        //disable end button and enable start button
+        e.target.disabled = true;
+        start.disabled = !start.disabled;
+      }
+    });
   });
-
-});
+};
